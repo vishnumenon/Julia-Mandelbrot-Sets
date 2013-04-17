346d8a34bc89e78143a710a904726b2b01af38f6
Vishnu Menon <me@vishnumenon.com>
2013-04-17 19:27:00 -0400

    Initial in rewrite

--- graphworker.js
+++ graphworker.js
@@ -1,74 +1,14 @@
-importScripts('assets/biginteger.js', 'assets/schemeNumber.js');
-
-function Grapher(type, a, b, width, height) {
-	this.sn = SchemeNumber;
-	this.fn = this.sn.fn;
-	this.type = type;
-	this.a = a;
-	this.b = b;
-	this.width = this.sn(width.toString());
-	this.height = this.sn(height.toString());
-	this.c = this.sn(this.a+"+"+this.b+"i");
-	this.z = null;
-	this.amin = this.sn("-10");
-	this.amax = this.sn("10");
-	this.bmin = this.sn("-10");
-	this.bmax = this.sn("10");
-}
-
-Grapher.prototype.translate = function(x, y) {
-	return {
-		a: this.fn["+"](
-			this.fn["*"](
-				this.fn["/"](
-					this.fn["-"](
-						this.amax,
-						this.amin),
-					this.width
-					),
-				x
-				),
-			this.amin
-			),
-		b: this.fn["+"](
-			this.fn["*"](
-				this.fn["/"](
-					this.fn["-"](
-						this.bmax,
-						this.bmin),
-					this.height
-					),
-				this.fn["-"](
-					this.height,
-					y
-					)
-				),
-			this.bmin
-			)
-	};
-};
-
-Grapher.prototype.getColor = function(point) {
-	return [Math.floor(Math.random() * (255 - 0 + 1)) + 0, Math.floor(Math.random() * (255 - 0 + 1)) + 0, Math.floor(Math.random() * (255 - 0 + 1)) + 0];
-};
-
-
-self.addEventListener('message', function(e){
-	var mje = new Grapher(e.data.type, e.data.a, e.data.b, e.data.width, e.data.height);
-	var buf8 = new Uint8ClampedArray(new ArrayBuffer(e.data.end - e.data.start));
-	var startY = e.data.start / e.data.width ;
-	var startX = e.data.start - (startY - e.data.width);
-	var endY = e.data.end / e.data.width;
-	var endX = e.data.end - (endY - e.data.width);
-	for(var x = startX; x <= endX; x++) {
-		for(var y = startY; y <= endY; y++) {
-			var index = ((y * 4 * e.data.width) + x * 4);
-			color  = mje.getColor(mje.translate(mje.sn(x.toString()), mje.sn(y.toString())));
-			buf8[index] = color[0];
-			buf8[++index] = color[1];
-			buf8[++index] = color[2];
-			buf8[++index] = 255;
-		}
+self.addEventListener('message', function(request) {
+	var g = 255;
+	var row = request.data.row;
+	var data = [];
+	for(var i = 3; i < request.data.width * 4; i +=4) {
+		data[i] = 255;
 	}
-	postMessage({data: buf8, start: e.data.start});
-}, false);
+	for(var i = 1 + Math.round(Math.random()); i < request.data.width * 4; i +=4) {
+		data[i] = g;
+	}
+
+	postMessage({data: data, row: row});
+
+});
--- index.js
+++ index.js
@@ -1,108 +1,61 @@
-// var mje;
-
-// $(document).ready(function() {
-// 	$('#explore_button').on('click', function(e) {
-// 		e.preventDefault();
-// 		$('#start').hide();
-// 		$('#explorer_graph').attr('width', $(window).width());
-// 		$('#explorer_graph').attr('height', $(window).height());
-// 		var workers = [];
-// 		var workerBounds = [];
-// 		var width = $('#explorer_graph')[0].width;
-// 		var height = $('#explorer_graph')[0].height;
-// 		var pixelArrayLength = width * height * 4;
-// 		var interval = pixelArrayLength / 16;
-// 		for(var i = 0; i<16; i++) {
-// 			workers[i] = new Worker("graphworker.js");
-// 			workerBounds[i] = {start: i * interval, end: ((i+1) * interval) - 1};
-// 			workers[i].addEventListener('message', function(e) {
-// 				console.log(e.data);
-// 				graph(e.data.data, e.data.start, $('#explorer_graph')[0], $('#explorer_graph')[0].getContext('2d'));
-// 			}, false);
-// 			workers[i].onerror = function(event){
-// 				throw new Error(event.message + " (" + event.filename + ":" + event.lineno + ")");
-// 			};
-// 			workers[i].postMessage({start: workerBounds[i].start, end: workerBounds[i].end, type: $("#equation").val(), a: $("#a").val(), b: $("#b").val(), width: $('#explorer_graph')[0].width, height: $('#explorer_graph')[0].height});
-// 		}
-
-// 		return false;
-// 	});
-// });
-
-// function graph(data, offset, canvas, context) {
-// 	var imgdata = context.getImageData(0, 0, width, height);
-// 	imgdata.data.set(data, offset);
-// 	context.putImageData(imgdata, 0, 0);
-// }
+var MJE;
 
 $(document).ready(function() {
-$('#start').hide();
-var complex = function() {
-    var that = {};
-    that.add = function( a, b ) {
-	return { r: a.r + b.r,
-		 i: a.i + b.i };
-    }
+	$('#explore_button').on('click', function(e){
+		MJE = {};
+		e.preventDefault();
+		MJE.width = $(window).width();
+		MJE.height = $(window).height();
+		MJE.amin  = -2;
+		MJE.amax  = 2;
+		MJE.bmin  = -2;
+		MJE.bmax  = 2;
+		MJE.canvas = $('#explorer_graph')[0];
+		MJE.ctx = MJE.canvas.getContext('2d');
+		MJE.canvas.setAttribute('width', MJE.width);
+		MJE.canvas.setAttribute('height', MJE.height);
+		MJE.type = $('#equation').val();
+		$('#start').hide();
+		drawGraph();
+		return false;
+	});
+});
 
-    that.subtract = function( a, b ) {
-	return { r: a.r - b.r, i: a.i - b.i };
-    }
+function drawGraph() {
+	MJE.rows = MJE.height;
+	for(var i = 0; i<10; i++) {
+		var worker = new Worker("graphworker.js");
+		worker.addEventListener('message', createCallback(worker));
+		worker.postMessage({
+			amin: MJE.amin,
+			amax: MJE.amax,
+			bmin: MJE.bmin,
+			bmax: MJE.bmax,
+			row: --MJE.rows,
+			type: MJE.type,
+			width: MJE.width
+		});
 
-    that.multiply = function( a,b ) {
-	return {r: a.r*b.r - a.i*b.i, 
-		i: a.i*b.r + a.r*b.i };
-    }
-
-    that.toString = function(a) {
-	return "" + a.r + "+" + a.i + "i";
-    }
-
-    that.abs = function(a) {
-	return Math.sqrt( a.r*a.r + a.i*a.i );
-    }
-
-    that.abs2 = function(a) {
-	return a.r*a.r + a.i*a.i;
-    }
-
-    return that;
-};
-var Complex = complex();
-
-var width = 600;
-var height = 400;
-
-var c = document.getElementById("explorer_graph");
-var ctx = c.getContext("2d");
-c.width = width;
-c.height = height;
-
-
-var imgData = ctx.createImageData(width,height);
-var i;
-var row, column;
-var topLeft = {r: -2, i: 1};
-var bottomRight = {r:1,i:-1};
-var drow = (bottomRight.i - topLeft.i)/height;
-var dcol = (bottomRight.r - topLeft.r)/width;
-var iter;
-var maxIter = 300;
-for( row = 0; row < height; row++ ) {
-    for (column = 0; column < width; column++ ) {
-	var p = {r: topLeft.r + column*dcol, i: topLeft.i + row*drow };
-	var v = {r: 0, i: 0}
-	iter = 0;
-	while ( Complex.abs2(v) < 4 && iter < maxIter) {
-	    v = Complex.add( Complex.multiply( v, v), p);
-	    iter++;
 	}
 
-	var index = (row*width + column)*4;
-	imgData.data[index+0] = iter % 255;
-	imgData.data[index+1] = iter % 255;
-	imgData.data[index+2] = iter % 255;
-	imgData.data[index+3] = 255;
-    }
-    ctx.putImageData(imgData, 0,0 );
 }
-});
+
+function createCallback(worker) {
+	return function(response) {
+		console.log(response);
+		var imageData = MJE.ctx.createImageData(MJE.width, 1);
+		imageData.data.set(response.data.data);
+		MJE.ctx.putImageData(imageData, 0, response.data.row);
+		if(MJE.rows > 0) {
+			worker.postMessage({
+				amin: MJE.amin,
+				amax: MJE.amax,
+				bmin: MJE.bmin,
+				bmax: MJE.bmax,
+				row: --MJE.rows,
+				type: MJE.type,
+				width: MJE.width
+			});
+		}
+	};
+}
