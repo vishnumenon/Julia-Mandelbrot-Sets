f0c68e08b71dcee4e46e3904f4bae305a43c5517
Vishnu Menon <me@vishnumenon.com>
2013-04-21 15:24:01 -0400

    Draws Mandelbrot, Dat.gui, zoom rectangles!

--- graphworker.js
+++ graphworker.js
@@ -1,14 +1,63 @@
+function Complex(a, b) {
+	this.a = a;
+	this.b = b;
+}
+
+Complex.prototype.plus =  function(c) {
+	return new Complex(this.a + c.a, this.b + c.b);
+};
+
+Complex.prototype.squared = function() {
+	return new Complex(this.a*this.a - this.b*this.b, this.a*this.b + this.a*this.b);
+};
+
+Complex.prototype.abs = function() {
+	return Math.sqrt(this.a*this.a + this.b*this.b);
+};
+
+function getA(x, min, max, width) {
+	return (x * ((max - min) / width)) + min;
+}
+
+function getB(y, min, max, height) {
+	return ((height - y) * ((max - min)/height)) + min;
+}
+
+function getColor(c, d1, d2, c1) {
+	var z = new Complex(0,0);
+	var counter = 0;
+	while(z.abs() < 2 && counter <= 100) {
+		if(counter === 100) {
+			return c1;
+		}
+		z = (z.squared()).plus(c);
+		counter++;
+	}
+	return [d1[0] + (counter / 100) * (d2[0] - d1[0]),
+		d1[1] + (counter / 100) * (d2[1] - d1[1]),
+		d1[2] + (counter / 100) * (d2[2] - d1[2]),
+		255];
+}
+
+
+
+
+
+
+
 self.addEventListener('message', function(request) {
-	var g = 255;
 	var row = request.data.row;
 	var data = [];
-	for(var i = 3; i < request.data.width * 4; i +=4) {
-		data[i] = 255;
+	var color;
+	for(var i = 0; i < request.data.width; i++) {
+		color = getColor(new Complex(
+			getA(i, request.data.amin, request.data.amax, request.data.width),
+			getB(row, request.data.bmin, request.data.bmax, request.data.height)), request.data.divergesColor1, request.data.divergesColor2, request.data.convergesColor);
+		data[4*i] = color[0];
+		data[4*i+1] = color[1];
+		data[4*i+2] = color[2];
+		data[4*i+3] = color[3];
 	}
-	for(var i = 1 + Math.round(Math.random()); i < request.data.width * 4; i +=4) {
-		data[i] = g;
-	}
-
 	postMessage({data: data, row: row});
 
 });
--- index.js
+++ index.js
@@ -10,16 +10,84 @@ $(document).ready(function() {
 		MJE.amax  = 2;
 		MJE.bmin  = -2;
 		MJE.bmax  = 2;
+		MJE.colors = {};
+		MJE.divergesColor1 = [255,255,255,255];
+		MJE.divergesColor2 = [0,0,0,255];
+		MJE.convergesColor = [100,100,100,255];
 		MJE.canvas = $('#explorer_graph')[0];
+		MJE.controlCanvas = $('#controls')[0];
 		MJE.ctx = MJE.canvas.getContext('2d');
+		MJE.cctx = MJE.controlCanvas.getContext('2d');
 		MJE.canvas.setAttribute('width', MJE.width);
 		MJE.canvas.setAttribute('height', MJE.height);
+		MJE.controlCanvas.setAttribute('width', MJE.width);
+		MJE.controlCanvas.setAttribute('height', MJE.height);
 		MJE.type = $('#equation').val();
+		MJE.drawing = false;
+		MJE.drawStart = {a: 0, b: 0};
+		MJE.redraw = function() { drawGraph(); };
+		MJE.reset = function() {
+			MJE.amin = -2;
+			MJE.amax = 2;
+			MJE.bmin = -2;
+			MJE.bmax = 2;
+			MJE.divergesColor1 = [255,255,255,255];
+			MJE.divergesColor2 = [0,0,0,255];
+			MJE.convergesColor = [100,100,100,255];
+			drawGraph();
+		};
 		$('#start').hide();
 		drawGraph();
+
+		gui = new dat.GUI();
+		gui.addColor(MJE, 'divergesColor1').listen();
+		gui.addColor(MJE, 'divergesColor2').listen();
+		gui.addColor(MJE, 'convergesColor').listen();
+		gui.add(MJE, 'redraw');
+		gui.add(MJE, 'reset');
+
 		return false;
 	});
+	$('#toImage').on('click',function(e){
+		var url = MJE.canvas.toDataURL();
+		w = window.open('saveImage.html');
+		w.onload = function(){
+			var placeholder = w.document.getElementById("fractalImage");
+			placeholder.src = url;
+		};
+	});
+	$('#controls').on('mousedown', function(e) {
+		MJE.drawing = true;
+		MJE.drawStart.x = e.pageX;
+		MJE.drawStart.y = e.pageY;
+		return false;
+	});
+	$('#controls').on('mousemove', function(e) {
+		if(MJE.drawing) {
+			MJE.cctx.clearRect(0,0,MJE.width, MJE.height);
+			MJE.cctx.globalAlpha = 0.3,
+			MJE.cctx.fillStyle = '#ffffff';
+			MJE.cctx.fillRect(Math.min(MJE.drawStart.x, e.pageX), Math.min(MJE.drawStart.y, e.pageY), Math.max(MJE.drawStart.x, e.pageX) - Math.min(MJE.drawStart.x, e.pageX), Math.max(MJE.drawStart.y, e.pageY) - Math.min(MJE.drawStart.y, e.pageY));
+		}
+	});
+	$('#controls').on('mouseup', function(e) {
+		MJE.amin = getA(Math.min(MJE.drawStart.x, e.pageX), MJE.amin, MJE.amax, MJE.width);
+		MJE.amax = getA(Math.max(MJE.drawStart.x, e.pageX), MJE.amin, MJE.amax, MJE.width);
+		MJE.bmin = getB(Math.max(MJE.drawStart.y, e.pageY), MJE.bmin, MJE.bmax, MJE.height);
+		MJE.bmax = getB(Math.min(MJE.drawStart.y, e.pageY), MJE.bmin, MJE.bmax, MJE.height);
+		MJE.cctx.clearRect(0,0,MJE.width, MJE.height);
+		MJE.drawing = false;
+		drawGraph();
+	});
 });
+
+function getA(x, min, max, width) {
+	return (x * ((max - min) / width)) + min;
+}
+
+function getB(y, min, max, height) {
+	return ((height - y) * ((max - min)/height)) + min;
+}
 
 function drawGraph() {
 	MJE.rows = MJE.height;
@@ -33,7 +101,11 @@ function drawGraph() {
 			bmax: MJE.bmax,
 			row: --MJE.rows,
 			type: MJE.type,
-			width: MJE.width
+			width: MJE.width,
+			height: MJE.height,
+			divergesColor1: MJE.divergesColor1,
+			divergesColor2: MJE.divergesColor2,
+			convergesColor: MJE.convergesColor
 		});
 
 	}
@@ -42,7 +114,6 @@ function drawGraph() {
 
 function createCallback(worker) {
 	return function(response) {
-		console.log(response);
 		var imageData = MJE.ctx.createImageData(MJE.width, 1);
 		imageData.data.set(response.data.data);
 		MJE.ctx.putImageData(imageData, 0, response.data.row);
@@ -54,7 +125,11 @@ function createCallback(worker) {
 				bmax: MJE.bmax,
 				row: --MJE.rows,
 				type: MJE.type,
-				width: MJE.width
+				width: MJE.width,
+				height: MJE.height,
+				divergesColor1: MJE.divergesColor1,
+				divergesColor2: MJE.divergesColor2,
+				convergesColor: MJE.convergesColor
 			});
 		}
 	};
